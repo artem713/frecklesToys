4e764eeba6872651b8da79e93a8ecc206ff5b9ef
artem713 <art.trubitsyn@gmail.com>
2014-10-19 20:38:51 +0300

    Changed main controller and main page view

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,9 +9,15 @@
  */
 angular.module('frecklesToysApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  	var alt = "Игрушки от Веснушки";
+    $scope.mainImages = [{
+    	"src": "http://cs617226.vk.me/v617226514/1f4d3/gf8r-m08Djc.jpg",
+    	"alt": alt
+    }, {
+    	"src": "http://cs605426.vk.me/v605426565/7d2e/HKBAHFnHyrA.jpg",
+    	"alt": alt
+    }, {
+    	"src": "http://cs616630.vk.me/v616630565/14b42/GhtDsLFCvYE.jpg",
+    	"alt": alt
+    }];
   });
