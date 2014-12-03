99857f5ed9ba0a2d1774777cd421bbdf569d27c8
artem713 <art.trubitsyn@gmail.com>
2014-12-03 23:45:19 +0200

    changin images on main page

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,17 +8,28 @@
  * Controller of the frecklesToysApp
  */
 angular.module('frecklesToysApp')
-  .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', function ($scope, $http, $interval, $timeout) {
+    $http.get("toys/main.json").success(function (toys) {
+      $scope.toys = toys;
+    });
+    var getRandomInt = function(min, max) {
+      return Math.floor(Math.random() * (max - min + 1)) + min;
+    };
     $scope.mainImage = "d_B_jvca2bw.jpg";
-  	var alt = "Игрушки от Веснушки";
-    $scope.mainToys = [{
-    	"src": "http://cs617226.vk.me/v617226514/1f4d3/gf8r-m08Djc.jpg",
-    	"alt": alt
-    }, {
-    	"src": "http://cs605426.vk.me/v605426565/7d2e/HKBAHFnHyrA.jpg",
-    	"alt": alt
-    }, {
-    	"src": "http://cs616630.vk.me/v616630565/14b42/GhtDsLFCvYE.jpg",
-    	"alt": alt
-    }];
-  });
+
+    $interval(function(){
+      var randomIndex = getRandomInt(0, $scope.toys.length - 1);
+      var randomToy = $scope.toys[randomIndex];
+      if ($scope.mainImage === randomToy.img.name) {
+        return;
+      } else {
+        $scope.myVar = "myClass1";
+        $timeout(function() {
+          $scope.myVar = "myClass2";
+          $scope.mainImage = randomToy.img.name;
+        }, 1000);
+      }
+      console.log(randomToy.img.name);
+      //$scope.mainImageSrc = "#/category/" + randomToy.category + "/" + randomToy.id;
+    }, 5000);
+});
